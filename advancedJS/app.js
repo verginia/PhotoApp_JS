f45ab8f1fe8f0f8b403bcafbcc014133c05ee9ac
verginia <geenika@gmail.com>
2025-05-15 21:10:48 +0300

    Task2: Created dataHandler, photoGrid, thumbnail js files, added the json file with the necessary information about image.

--- advancedJS/app.js
+++ advancedJS/app.js
@@ -1,13 +1,24 @@
 import Title from "./components/title.mjs";
+import PhotoGrid from "./components/photoGrid.mjs";
+import DataHandler from "./components/dataHandler.mjs";
 
 class App{
-    render(container){
+    constructor() {
+        this.data = [];
+    }
+
+
+    async render(container){
+        this.data = await DataHandler.fetchData();
+
+
         const main = document.createElement("main");
         const documentFragment = document.createDocumentFragment();
 
         main.classList.add("app");
         main.innerHTML = `
             ${new Title().render()}
+            ${new PhotoGrid(this.data).render()}
         `;
 
         documentFragment.appendChild(main);
